1ff82299d7a07015f860d49f9746b344a1228a8b
risingtalent0609 <bsbussiness000@gmail.com>
2022-10-21 04:52:34 +0900

    yeah.

--- src/components/Ecosystem/Ecosystem.js
+++ src/components/Ecosystem/Ecosystem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import EcosystemItem from "../EcosystemItem/EcosystemItem";
 const Ecosystem = () => {
