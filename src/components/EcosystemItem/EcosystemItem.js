4b68465c5b13b8486f00a90eb3f3685df807e29a
risingtalent0609 <bsbussiness000@gmail.com>
2022-10-21 04:39:13 +0900

    router

--- src/components/EcosystemItem/EcosystemItem.js
+++ src/components/EcosystemItem/EcosystemItem.js
@@ -5,7 +5,7 @@ import "./EcosystemItem.scss";
 const EcosystemItem = (props) => {
   return (
     <div className="item-control">
-      <Link to={props.alink}>
+      <Link to={"/" + props.alink}>
         <img src={props.image} alt="item" />
         <h4 className="text-center">{props.text}</h4>
       </Link>
