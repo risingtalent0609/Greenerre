cf7d528259187bba91b100c1ff7ebf8301e0e730
risingtalent0609 <bsbussiness000@gmail.com>
2022-10-21 10:32:33 +0900

    update for alink 9

--- src/components/EcosystemItem/EcosystemItem.js
+++ src/components/EcosystemItem/EcosystemItem.js
@@ -5,7 +5,7 @@ import "./EcosystemItem.scss";
 const EcosystemItem = (props) => {
   return (
     <div className="item-control">
-      {props.type == "local" ? (
+      {props.type === "local" ? (
         <Link to={props.alink}>
           <img src={props.image} alt="item" />
           <h4 className="text-center">{props.text}</h4>
